56eef083165870158cbe037e0c4196d49fb8a83b
Dimitris Bounias <bounias.dimitris@gmail.com>
2019-08-09 12:48:35 +0300

    Make icon bigger

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
 	QApplication a(argc, argv);
 	a.setAttribute(Qt::AA_EnableHighDpiScaling, true);
-    a.setWindowIcon(QIcon(":/mll_icon.ico"));
+    a.setWindowIcon(QIcon(":/mll_icon2.ico"));
 	MainWindow w;
 	w.show();
 
